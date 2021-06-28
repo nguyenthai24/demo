30d2a683370a71e6fc2f8e94efd8f64b0678217c
Admin <nguyenvanthai.humg24@gmail.com>
2021-06-28 20:36:57 +0700

    commit add product

---
+++ database/migrations/2021_06_28_132957_create_tbl_category.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_category', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('category_title');
+            $table->double('category_price');
+            $table->string('category_desc');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_category');
+    }
+}
